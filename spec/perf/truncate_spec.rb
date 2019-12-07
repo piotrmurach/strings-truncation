846cf05511e7bf86fb9d5525a5e65e254c8a2949
Piotr Murach <pmurach@gmail.com>
2019-12-07 18:11:42 +0000

    Add perf test

---
+++ spec/perf/truncate_spec.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+require "rspec-benchmark"
+require "active_support/core_ext/string/filters"
+
+RSpec.describe Strings::Truncation do
+  include RSpec::Benchmark::Matchers
+
+  it "pluralizes nouns as fast as ActiveSupport" do
+    text = "It is not down on any map; true places never are."
+    expect {
+      Strings::Truncation.truncate(text, 20)
+    }.to perform_slower_than {
+      text.truncate(20)
+    }.at_most(220).times
+  end
+
+  it "allocates no more than 132 objects" do
+    text = "It is not down on any map; true places never are."
+    expect {
+      Strings::Truncation.truncate(text)
+    }.to perform_allocation(132)
+  end
+end
