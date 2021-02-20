40671d7f764efd4c14721b5a4d0bfc61bcbe0a5a
Piotr Murach <pmurach@gmail.com>
2021-02-20 23:36:44 +0100

    Change performance tests to update speed and allocation limits

--- spec/perf/truncate_spec.rb
+++ spec/perf/truncate_spec.rb
@@ -6,19 +6,19 @@ require "active_support/core_ext/string/filters"
 RSpec.describe Strings::Truncation do
   include RSpec::Benchmark::Matchers
 
-  it "pluralizes nouns as fast as ActiveSupport" do
+  it "truncates text slower than ActiveSupport" do
     text = "It is not down on any map; true places never are."
     expect {
       Strings::Truncation.truncate(text, 20)
     }.to perform_slower_than {
       text.truncate(20)
-    }.at_most(150).times
+    }.at_most(135).times
   end
 
-  it "allocates no more than 107 objects" do
+  it "allocates no more than 95 objects" do
     text = "It is not down on any map; true places never are."
     expect {
       Strings::Truncation.truncate(text, 20)
-    }.to perform_allocation(107)
+    }.to perform_allocation(95).objects
   end
 end
